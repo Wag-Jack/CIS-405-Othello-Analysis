f53e04f6ec07cae9480c26fa4e31b2e1755ef3f8
Jack Wagner <wagjack@umich.edu>
2024-10-18 18:02:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Othello AI Opponent#
+##CIS 405-001 | Professor Steiner | Fall 2024##
+##Created by Jack Wagner##
---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+def main():
+    print("This will be the implementation of an Othello game.")
+
+
+if __name__ == "__main__":
+    main()
